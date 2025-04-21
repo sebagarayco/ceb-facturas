fe6022a16ed83dae3ca1de6cafda2e8e0f71a5c8
sebagarayco <sebagarayco@gmail.com>
2025-04-21 14:48:58 -0300

    fix import error

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-from pdf_extractor import extract_text_from_pdf
 from google.oauth2.service_account import Credentials
 
 # Obtener credenciales desde variables de entorno
